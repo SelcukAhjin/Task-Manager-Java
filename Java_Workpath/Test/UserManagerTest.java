5ae1592e15bd07c4a2a53e60ae0071cbd99caef2
Ilker Selcuk Akyüz <selcukilkerakyuez@gmail.com>
2026-05-04 13:15:59 +0200

    Initial Commit | Phase 1 abgeschlossen
    Just added a small Refactor: complete DAO pattern integration and initialize JUnit 5 test suite
    
    Total working time: 62 Hours

--- Java_Workpath/Test/UserManagerTest.java
+++ Java_Workpath/Test/UserManagerTest.java
@@ -1,14 +1,23 @@
 package Java_Workpath.Test;
 
+import Java_Workpath.projekt.UserManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserManagerTest {
+    UserManager um = new UserManager();
+
 
     @org.junit.jupiter.api.Test
     void isValidEmail() {
+        assertTrue(um.isValidEmail("dwf@wa"));
+        assertFalse(um.isValidEmail("sefdw"));
     }
 
     @org.junit.jupiter.api.Test
     void isValidPassword() {
+        assertFalse(um.isValidPassword(""));
+        assertTrue(um.isValidPassword("passwo"));
+        assertFalse(um.isValidPassword("passw"));
     }
 }
